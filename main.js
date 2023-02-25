da692d4419df0219b5353ea194686a1210aace7d
dejourford <dejourford@my.unt.edu>
2023-02-25 17:31:45 -0600

    add styles for whole page and add js for modal

---
+++ main.js
@@ -0,0 +1,14 @@
+// MODAL
+var modal = document.getElementById("modal");
+var enterBtn = document.querySelector('.enterBtn');
+
+
+enterBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
