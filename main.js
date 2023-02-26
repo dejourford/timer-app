ff1aa9b032058367fc643ffec0d5bffaf4f879f8
dejourford <dejourford@my.unt.edu>
2023-02-26 03:59:51 -0600

    create function for decrement

--- main.js
+++ main.js
@@ -7,19 +7,30 @@ enterBtn.onclick = function() {
   modal.style.display = "none";
 }
 
-// DEFINE A SECOND
-const now = new Date()
-const second = now.getSeconds();
-
 
 
 // CREATE FUNCTION TO GET USER TIME
 getTime = () => {
     const userTime = document.querySelector('.time-input').value
  // INSERT TIME IN DOM
- const minutesTime = document.querySelector('#minutesTime')
+ let minutesTime = document.querySelector('#minutesTime')
  minutesTime.textContent = userTime + ":" + "0" + "0"
  userTimeToNumber = parseInt(minutesTime.textContent)
+//  CONVERT USER TIME IN MINUTES TO SECONDS
+ userTimeInSeconds(userTimeToNumber)
+ let numUserTimeInSeconds = Number(userTimeInSeconds)
+ 
+//  function for second decrement
+ let num = 60;
+
+ let intervalId = setInterval(function() {
+   num--;
+   console.log(num); // Output: the current value of `num` after decrementing
+ 
+   if (num <= 0) {
+     clearInterval(intervalId); // Stop the loop when `num` reaches 0
+   }
+ }, 1000);
 
 }
 
@@ -29,3 +40,13 @@ submitBtn.addEventListener('click', () => {
     getTime()
    
 })
+
+// DEFINE A SECOND
+const now = new Date()
+const second = now.getSeconds();
+// DEFINE USER TIME IN SECONDS
+const userTimeInSeconds = (userTimeToNumber) => {
+    return userTimeToNumber * 60
+}
+
+
