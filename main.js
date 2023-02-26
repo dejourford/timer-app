cc5d32eeee522c2e9f4586b3b5c5c75c8f45fbbc
dejourford <dejourford@my.unt.edu>
2023-02-25 20:54:23 -0600

    removed function allowing exit click outside modal

--- main.js
+++ main.js
@@ -7,12 +7,6 @@ enterBtn.onclick = function() {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 // DEFINE A SECOND
 const now = new Date()
 const second = now.getSeconds();
