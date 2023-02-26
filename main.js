ea52b96f0838cdcf416ed4454c0122115a9764dd
dejourford <dejourford@my.unt.edu>
2023-02-26 12:10:23 -0600

    create function for formatting time

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ enterBtn.onclick = function() {
   modal.style.display = "none";
 }
 
+// DEFINE USER TIME IN SECONDS
+const userTimeInSeconds = (userTimeToNumber) => {
+    return userTimeToNumber * 60
+}
+
+
+// LISTEN FOR SUBMIT BUTTON CLICK
+const submitBtn = document.querySelector('.set-time-btn')
+submitBtn.addEventListener('click', () => {
+    getTime()
+   
+})
+
+
+// FUNCTION FOR TURNING SECONDS INTO MINUTE-SECOND FORMAT
+function formatTime(seconds) {
+    var minutes = Math.floor(seconds / 60);
+    var remainingSeconds = seconds % 60;
+    return minutes + ":" + (remainingSeconds < 10 ? "0" : "") + remainingSeconds;
+  }
+
+
 
 
 // CREATE FUNCTION TO GET USER TIME
@@ -18,15 +40,19 @@ getTime = () => {
  userTimeToNumber = parseInt(minutesTime.textContent)
 //  CONVERT USER TIME IN MINUTES TO SECONDS
  userTimeInSeconds(userTimeToNumber)
- let numUserTimeInSeconds = Number(userTimeInSeconds)
+ let numUserTimeInSeconds = Number(userTimeInSeconds);
  
-//  function for second decrement
- let num = 60;
+
+
+
+ //  function for second decrement
+ let num = userTime * 60;
 
  let intervalId = setInterval(function() {
    num--;
    console.log(num); // Output: the current value of `num` after decrementing
- 
+    minutesTime.textContent = formatTime(num)
+
    if (num <= 0) {
      clearInterval(intervalId); // Stop the loop when `num` reaches 0
    }
@@ -34,19 +60,6 @@ getTime = () => {
 
 }
 
-// LISTEN FOR SUBMIT BUTTON CLICK
-const submitBtn = document.querySelector('.set-time-btn')
-submitBtn.addEventListener('click', () => {
-    getTime()
-   
-})
 
-// DEFINE A SECOND
-const now = new Date()
-const second = now.getSeconds();
-// DEFINE USER TIME IN SECONDS
-const userTimeInSeconds = (userTimeToNumber) => {
-    return userTimeToNumber * 60
-}
 
 
